1d3e5497bdfb08f0675f961400e3c7495d2287d3
samuelgerard <goobster3367@gmail.com>
2021-04-10 14:28:20 -0700

    Version 1.0.0 done, turn into executable

---
+++ KivyApp.py
@@ -0,0 +1,90 @@
+import kivy
+from kivy.app import App #<----- builds the window and the graphics of the module
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+from kivy.uix.floatlayout import FloatLayout
+
+from katakana_shitpost_bot import *
+
+# class Touch(Widget):
+
+# 	btn = ObjectProperty(None)
+
+# 	def on_touch_down(self, touch):
+# 		print("")
+# 		self.btn.opacity = 0.5
+
+# 	def on_touch_move(self, touch):
+# 		print("Mouse Move", touch)
+
+# 	def on_touch_up(self, touch):
+# 		print("Mouse Up", touch)
+# 		self.btn.opacity = 1
+
+class MyGrid(Widget):
+	inputted_text = ObjectProperty(None)
+	output_text = ObjectProperty(None)
+	katakana_translator = katakana_meme_bot()
+	return_string = ""
+
+
+	def btn(self):
+		print(f"Inputted_text: {self.inputted_text.text}")
+		temp_string = self.inputted_text
+		print(f'the string is:{temp_string.text}')
+		li = list(temp_string.text.split(" "))
+		self.return_string = self.katakana_translator.Activate_shitpost(li)
+		
+		print(str(" ".join(self.return_string)))
+
+		self.output_text.text = str(" ".join(self.return_string))
+
+
+
+
+
+
+
+# class MyGrid(GridLayout):
+# 	def __init__(self, **kwargs):
+# 		super(MyGrid, self).__init__(**kwargs)
+# 		self.cols = 1
+
+# 		self.inside = GridLayout()
+# 		self.inside.cols = 2
+
+# 		self.inside.add_widget(Label(text = "String Input: "))
+# 		self.String_Input = TextInput(multiline = False)
+# 		self.inside.add_widget(self.String_Input)
+
+# 		self.inside.add_widget(Label(text = "kekw: "))
+# 		self.kekw = TextInput(multiline = False)
+# 		self.inside.add_widget(self.kekw)
+
+
+# 		self.submit = Button(text="Submit For Memes", font_size = 40)
+# 		self.submit.bind(on_press=self.pressed)
+# 		self.add_widget(self.submit)
+
+# 		self.add_widget(self.inside)
+
+# 	def pressed(self, instance):
+# 		String_to_be_processed = self.String_Input.text
+
+# 		print(String_to_be_processed)
+
+# 		self.String_Input.text = ""
+
+
+class MyApp(App):
+	def build(self):
+		self.title = "Full Katakana Translator"
+		return MyGrid()
+
+if __name__ == '__main__':
+	MyApp().run()
--- katakana_shitpost_bot.py
+++ katakana_shitpost_bot.py
@@ -8,86 +8,71 @@ MAX = 10
 example = ['this','is','max','ten','words','in','one','url','call','max','this','is','max','ten','words','in','one','url','call','max']
 example2 = ['this','is','max']
 
-# URL = f'https://www.sljfaq.org/cgi/e2k.cgi?word={ex[0]}%20{ex[1]}%20{ex[2]}%20{ex[3]}%20{ex[4]}%20{ex[5]}%20{ex[6]}%20{ex[7]}%20{ex[8]}%20{ex[9]}'
-# UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
+class katakana_meme_bot(object):
 
 
-# opener = AppURLopener()
-# response = opener.open(URL)
+	def adjust_URL_call(self, ex):
+		base_url = f'https://www.sljfaq.org/cgi/e2k.cgi?word='
 
-# page=urllib.request.Request(URL, headers={'User-Agent':'Mozilla/5.0'})
-# infile=urllib.request.urlopen(page).read()
-# data = infile.decode('utf-8')
+		for words in ex:
+			append_argument = words + '%' + '20'
+			base_url = base_url + append_argument
+			print(base_url)
 
-# soup = BeautifulSoup(response.read())
+		return base_url
 
-# tags = soup.find_all('td', {'class':'j_pron_spell'})
+	def make_URL_call(self, ex, current_list):
 
-# for thats_cap in tags:
-# 	print(', '.join(thats_cap.contents))
+		if len(ex) != 10:
+			URL = self.adjust_URL_call(ex)
+		else:
+			URL = f'https://www.sljfaq.org/cgi/e2k.cgi?word={ex[0]}%20{ex[1]}%20{ex[2]}%20{ex[3]}%20{ex[4]}%20{ex[5]}%20{ex[6]}%20{ex[7]}%20{ex[8]}%20{ex[9]}'
 
-def adjust_URL_call(ex):
-	base_url = f'https://www.sljfaq.org/cgi/e2k.cgi?word='
+		opener = AppURLopener()
+		response = opener.open(URL)
 
-	for words in ex:
-		append_argument = words + '%' + '20'
-		base_url = base_url + append_argument
-		print(base_url)
+		soup = BeautifulSoup(response.read())
 
-	return base_url
+		tags = soup.find_all('td', {'class':'j_pron_spell'})
 
-def make_URL_call(ex, current_list):
+		for thats_cap in tags:
+			current_list.append(', '.join(thats_cap.contents))
 
-	if len(ex) != 10:
-		URL = adjust_URL_call(ex)
-	else:
-		URL = f'https://www.sljfaq.org/cgi/e2k.cgi?word={ex[0]}%20{ex[1]}%20{ex[2]}%20{ex[3]}%20{ex[4]}%20{ex[5]}%20{ex[6]}%20{ex[7]}%20{ex[8]}%20{ex[9]}'
-
-	opener = AppURLopener()
-	response = opener.open(URL)
-
-	soup = BeautifulSoup(response.read())
-
-	tags = soup.find_all('td', {'class':'j_pron_spell'})
-
-	for thats_cap in tags:
-		current_list.append(', '.join(thats_cap.contents))
-
-	return current_list
+		return current_list
 
 
-def Activate_shitpost(provided_string):
-	buffer_list = []
-	all_words = []
-	for traverse_string in provided_string:
-		buffer_list.append(traverse_string)
+	def Activate_shitpost(self, provided_string):
+		buffer_list = []
+		all_words = []
+		for traverse_string in provided_string:
+			buffer_list.append(traverse_string)
 
-		if len(buffer_list) == MAX:
-			all_words = make_URL_call(buffer_list, all_words)
+			if len(buffer_list) == MAX:
+				all_words = self.make_URL_call(buffer_list, all_words)
 
-			buffer_list = []
+				buffer_list = []
 
-	#if buffer list has any remaining words, we'll append the last of it
-	if buffer_list:
-		all_words = make_URL_call(buffer_list, all_words)
+		#if buffer list has any remaining words, we'll append the last of it
+		if buffer_list:
+			all_words = self.make_URL_call(buffer_list, all_words)
 
-	return all_words
-
-
-def main():
-	user_input = input(f'Enter The Shitpost:')
-	final_string = ""
-
-	li = list(user_input.split(" "))
-
-	final_string = Activate_shitpost(li)
-
-	print(' '.join(final_string))
-	print(f'length of the final string is :{len(final_string)}')
+		return all_words
 
 
-if __name__ == '__main__':
-	main()
+# def main():
+# 	user_input = input(f'Enter The Shitpost:')
+# 	final_string = ""
+
+# 	li = list(user_input.split(" "))
+
+# 	final_string = Activate_shitpost(li)
+
+# 	print(' '.join(final_string))
+# 	print(f'length of the final string is :{len(final_string)}')
+
+
+# if __name__ == '__main__':
+# 	main()
 
 
 
